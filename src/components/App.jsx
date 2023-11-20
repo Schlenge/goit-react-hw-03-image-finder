c11079433e1a4dae5e0984d2ee2ab5d418d9c4e6
Schlenge <emil.tarala@gmail.com>
2023-11-20 20:37:38 +0200

    commit 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ export const App = () => {
   return (
     <div>
       <h1 className={css.title}>React HW - Image finder</h1>
+      <h2 className={css.subtitle}>Pune osul la munca</h2>
     </div>
   );
 };
