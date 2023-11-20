604c83831bed8fd8ad56ac07d62af54cbe6fa820
Schlenge <emil.tarala@gmail.com>
2023-11-20 20:10:48 +0200

    commit 2 again

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import css from '../components/App.module.css';
 export const App = () => {
   return (
     <div>
-      <h1 className={CSS.title}>React HW - Image finder</h1>
+      <h1 className={css.title}>React HW - Image finder</h1>
     </div>
   );
 };
