9e47631189c23ebb5eaa95bd4ede45f577277a4a
Schlenge <emil.tarala@gmail.com>
2023-11-20 20:05:55 +0200

    commit 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import css from '../components/App.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1 className={CSS.title}>React HW - Image finder</h1>
     </div>
   );
 };
